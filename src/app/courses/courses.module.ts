4ba2cfad100ac0f27ab7e6ef595b12122d88e857
gianneves <gianneves@hotmail.com>
2024-02-27 15:56:05 -0300

    feat: Material Table CSS and Creating a Material App Module

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
-import { MatTableModule } from '@angular/material/table'
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
+
+
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatTableModule
+    AppMaterialModule
   ]
 })
 export class CoursesModule { }
--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 import { MatTableModule } from '@angular/material/table'
+import { MatCardModule } from '@angular/material/card'
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatCardModule, MatToolbarModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
