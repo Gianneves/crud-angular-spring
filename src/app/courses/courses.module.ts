074f048bf9ce84eb43b9e254fc27b3325e5d2507
gianneves <gianneves@hotmail.com>
2024-02-27 15:15:20 -0300

    feat: Creating Table of Materials to List Courses

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
-
+import { MatTableModule } from '@angular/material/table'
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    CoursesRoutingModule
+    CoursesRoutingModule,
+    MatTableModule
   ]
 })
 export class CoursesModule { }
--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+import { MatTableModule } from '@angular/material/table'
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
+
+  courses: Course[] = []
+  displayedColumns = ['name', 'category']
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
 
 }
